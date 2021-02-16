2533512ecea787f3f88795aee580e2af81e75031
EdwardCullen30 <f20180872@pilani.bits-pilani.ac.in>
2021-02-16 16:34:11 +0530

    initial_data_parsing_comment

---
+++ sp500.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import os
+import time
+from datetime import datetime
+
+path = "/Users/swapnilyadav/Desktop/Ml/Dataparsing/intraQuarter"
+
+
+def Key_Stats(gather="Total Debt/Equity (mrq)"):
+    statspath = path+'/_KeyStats'
+    stock_list = [x[0] for x in os.walk(statspath)]
+    df = pd.DataFrame(columns=['Date', 'Unix', 'Ticker', 'DE Ratio', 'Price', 'SP500'])
+
+    sp500_df = pd.read_csv("GSPC.csv")
+
+    for each_dir in stock_list[1:25]:
+        each_file = os.listdir(each_dir)
+        ticker = each_dir.split(
+            "/Users/swapnilyadav/Desktop/Ml/Dataparsing/intraQuarter/_KeyStats/")[1].split(':')[0]
+        if len(each_file) > 0:
+            for file in each_file:
+                date_stamp = datetime.strptime(file, '%Y%m%d%H%M%S.html')
+                unix_time = time.mktime(date_stamp.timetuple())
+                full_file_path = each_dir+'/'+file
+                source = open(full_file_path, 'r').read()
+                try:
+                    value = float(source.split(
+                        gather+':</td><td class="yfnc_tabledata1">')[1].split('</td>')[0])
+
+                    try:
+                        sp500_date = datetime.fromtimestamp(unix_time).strftime('%Y-%m-%d')
+                        row = sp500_df[sp500_df["Date"] == sp500_date]
+                        sp500_value = float(row["Adj Close"])
+                    except:
+                        sp500_date = datetime.fromtimestamp(unix_time-259200).strftime('%Y-%m-%d')
+                        row = sp500_df[sp500_df["Date"] == sp500_date]
+                        sp500_value = float(row["Adj Close"])
+
+                    stock_price = float(source.split('</small><big><b>')[1].split('</b></big>')[0])
+                    #print("stock_price:",stock_price,"ticker:", ticker)
+
+                    df = df.append({'Date': date_stamp,
+                                    'Unix': unix_time,
+                                    'Ticker': ticker,
+                                    'DE Ratio': value,
+                                    'Price': stock_price,
+                                    'SP500': sp500_value}, ignore_index=True)
+                except Exception as e:
+                    pass
+
+    save = gather.replace(' ', '').replace(')', '').replace('(', '').replace('/', '')+('.csv')
+    print(save)
+    df.to_csv(save)
+
+
+Key_Stats()
